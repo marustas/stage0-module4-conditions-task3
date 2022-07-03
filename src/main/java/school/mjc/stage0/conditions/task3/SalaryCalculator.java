47f4940954a452b5658d6b3c53f2306be7298f07
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-03 17:51:21 +0300

    Second try

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -3,11 +3,11 @@ package school.mjc.stage0.conditions.task3;
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
         if (salary <= 10000) {
-            salary *= 0.85;
+            System.out.println(salary * 0.85);
         } else if (salary > 10000 && salary <= 20000) {
-            salary *= 0.82;
+            System.out.println(salary * 0.82);
         } else if (salary > 20000) {
-            salary *= 0.80;
+            System.out.println(salary * 0.80);
         } else if (salary < 0) {
             System.out.println("wrong input");
         }
